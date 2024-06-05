9c7527ccdb3d30c4bcf25ebc1513d73daed9c5b9
Jim Verducci <jim@wristband.dev>
2024-06-05 12:10:54 -0700

    Add debug logs

--- src/auth/middleware-auth.ts
+++ src/auth/middleware-auth.ts
@@ -17,6 +17,9 @@ async function performTokenRefresh(refreshToken: string) {
   // This condition is here for local development when the Wristband platform is also local.  This
   // will never be the case in production.
   const protocol = process.env.NEXT_PUBLIC_TRUST_SELF_SIGNED_CERT ? 'http' : 'https';
+  console.log('TOKEN ENDPOINT: ', `${protocol}://${process.env.APPLICATION_DOMAIN}/api/v1/oauth2/token`);
+  console.log('TOKEN HEADERS: ', BASIC_AUTH_FETCH_HEADERS);
+  console.log('REFRESH TOKEN: ', refreshToken);
   const res = await fetch(`${protocol}://${process.env.APPLICATION_DOMAIN}/api/v1/oauth2/token`, {
     method: 'POST',
     headers: BASIC_AUTH_FETCH_HEADERS,
